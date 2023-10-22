26aa5b46f2137628d3d6f51b501736a0da11e92a
LimJung7050 <101217050@students.swinburne.edu.my>
2023-10-22 20:34:53 +0800

    Update index.php

--- login_CLTS/index.php
+++ login_CLTS/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<!-- title -->
-	<h1 class="system_title">Communication Logs Tracking System</h1>
+	<h1 class="system_title">ADAAA Communication Logs Tracking System</h1>
 
 	<!-- Form -->
 	<div class="login_form">
